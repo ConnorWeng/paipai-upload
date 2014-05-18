9d53961966d8e3ed5dc6b60540fef46017bc108a
Connor Weng <foamtea30@gmail.com>
2014-05-19 00:02:07 +0800

    Modify config.

--- Conf/config.php
+++ Conf/config.php
@@ -4,7 +4,7 @@ return array(
     'appOAuthId' => '700155306',
     'appOAuthKey' => 'i4qt0Uz2m2gK4XiP',
     'taobao_app_key' => '21545953',
-    'taobao_secret_key' => '6da0d607c561e9826e0ff7a8e1f92259'
+    'taobao_secret_key' => '6da0d607c561e9826e0ff7a8e1f92259',
 
     //数据库配置
     'DB_TYPE' => 'mysql',
